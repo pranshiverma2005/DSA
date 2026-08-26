0db63b463f459986402253ed9e30df4df709b010
Pranshi Verma <vermapranshi216@gmail.com>
2026-08-26 16:36:47 +0530

    Time: 2 ms (40.94%) | Memory: 44.2 MB (28.86%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // A beautiful substring must start with 1
+            if (s.charAt(i) == '0') {
+                continue;
+            }
+
+            int ones = 0;
+
+            for (int j = i; j < s.length(); j++) {
+
+                if (s.charAt(j) == '1') {
+                    ones++;
+                }
+
+                if (ones == k) {
+                    String curr = s.substring(i, j + 1);
+
+                    if (ans.equals("") ||
+                        curr.length() < ans.length() ||
+                        (curr.length() == ans.length() &&
+                         curr.compareTo(ans) < 0)) {
+
+                        ans = curr;
+                    }
+
+                    // Further extension will only increase length
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
